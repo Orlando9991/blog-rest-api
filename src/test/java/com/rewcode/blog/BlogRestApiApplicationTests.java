bf5b7be015fdf9b930feedc880ff229672777cb8
Orlando Cruz <orlandocruz999@gmail.com>
2024-02-14 13:58:59 +0000

    Initial commit

---
+++ src/test/java/com/rewcode/blog/BlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewcode.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
